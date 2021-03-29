fb073e7517aec3c170907431a6ed6b66f80ed5c9
dimas pratama <diemustpratama@gmail.com>
2021-03-29 10:31:30 +0700

    test environment

---
+++ app/src/main/java/com/digitalent/finalprojectdta/Activity/Login.java
@@ -0,0 +1,17 @@
+package com.digitalent.finalprojectdta.Activity;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.digitalent.finalprojectdta.R;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
--- app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
+++ app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
@@ -18,7 +18,7 @@ public class Splash extends AppCompatActivity {
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, Main.class);
+                Intent intent = new Intent(Splash.this, Login.class);
                 startActivity(intent);
                 finish();
             }
