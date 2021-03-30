0142ffd155a488d80d196487c45914690ac68b64
dimas pratama <diemustpratama@gmail.com>
2021-03-30 13:34:09 +0700

    fix force close

---
+++ app/src/main/java/com/digitalent/finalprojectdta/Activity/Register.java
@@ -0,0 +1,16 @@
+package com.digitalent.finalprojectdta.Activity;
+
+import android.os.Bundle;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.digitalent.finalprojectdta.R;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
--- app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
+++ app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
@@ -14,7 +14,7 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
