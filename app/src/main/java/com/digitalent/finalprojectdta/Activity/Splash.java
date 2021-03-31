6d5fc6a643b2ec0dbcaca8766686657e6d8415b2
dimas pratama <diemustpratama@gmail.com>
2021-03-31 09:07:37 +0700

    update login and register

--- app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
+++ app/src/main/java/com/digitalent/finalprojectdta/Activity/Splash.java
@@ -14,11 +14,11 @@ public class Splash extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash);
+        setContentView(R.layout.activity_splash_screen);
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
-                Intent intent = new Intent(Splash.this, Main.class);
+                Intent intent = new Intent(Splash.this, Login.class);
                 startActivity(intent);
                 finish();
             }
